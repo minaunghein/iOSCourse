980dee25dd09271119bac17fe33131cece59a064
Min Aung Hein <minaunghein@gmail.com>
2016-10-22 03:14:47 +0630

    Add StackView

--- 0104_UID_STK2_VoteNow/simplestack/ViewController.swift
+++ 0104_UID_STK2_VoteNow/simplestack/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var clintonLabel: UILabel!
 
+    @IBOutlet weak var trumpLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func voteClinton(_ sender: UIStepper) {
+        
+        clintonLabel.text = String(Int(sender.value * 100))
+        
+    }
+    @IBAction func voteDonald(_ sender: UIStepper) {
+        trumpLabel.text = String(Int(sender.value * 100))
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
