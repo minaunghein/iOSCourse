c88c5327a7606b70a03a5f2d66ecbb6be1ef3918
Min Aung Hein <minaunghein@gmail.com>
2016-10-30 06:35:15 +0630

    no message

--- 0100_Swift/MyPlaygroundLooping.playground/Contents.swift
+++ 0100_Swift/MyPlaygroundLooping.playground/Contents.swift
@@ -2,4 +2,47 @@
 
 import UIKit
 
-var str = "Hello, playground"
+
+//products 
+var products = ["Egg","Chicken","Pork","Beef","Fish"]
+for  data   in products
+{
+    print(data)
+}
+
+//Students Dict
+var dict = [ "A":"Apple"  , "B":"Bell", "C":"Cat"   ,"D":"Dragon"   ]
+var myArray = [String]()
+
+for (k,v) in dict
+{
+    print(k,v)
+    myArray.append(k)
+}
+
+print(myArray)
+var sorted = myArray.sorted()
+print(sorted)
+
+for i in sorted
+{
+    print(dict[i])
+}
+//--------
+
+var numArray = [ 10,20,30,40,50]
+
+var i = 0
+while  i < numArray.count   //0 - 4
+{
+    print(numArray[i])
+    numArray[i] = numArray[i] * 10
+    i = i + 1
+    if i > 2
+    {
+        break;
+    }
+}
+print(numArray)
+
+
--- 0100_Swift/MyPlaygroundSwitch.playground/Contents.swift
+++ 0100_Swift/MyPlaygroundSwitch.playground/Contents.swift
@@ -3,3 +3,20 @@
 import UIKit
 
 var str = "Hello, playground"
+
+
+var errorCode = 3
+
+
+
+switch (errorCode)
+{
+case 0:print("No error")
+       fallthrough
+case 1:print("File error")
+    fallthrough
+case 2:print("Network error")
+default:print("Unknown error")
+}
+print("End")
+
